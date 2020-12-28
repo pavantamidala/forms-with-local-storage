362c391db37f2a4914b28de990370020bdb2b2b3
pavantamidala <pavantamidala023@gmail.com>
2020-12-28 19:00:28 +0530

    third commit

--- script.js
+++ script.js
@@ -1,162 +1,163 @@
-let submit = document.querySelector('button')
-let inputs = document.querySelectorAll('input')
-let form = document.querySelector('form')
-let edit = document.querySelector('#edit')
+let inputs = document.querySelectorAll("input")
+let submitButton = document.querySelector(".submit")
 let container = document.querySelector(".container")
-let h2 = document.querySelector('h2')
-let deleteButton = document.querySelector("#delete")
-h2.style.display = "none"
-let formArr = []
-let genderObj = {}
-let userDetailObj;
-edit.style.display = "none"
-deleteButton.style.display = "none"
-submit.addEventListener("click", function (e) {
 
-    for (let input of inputs) {
-        if (input.className === "gender") {
-            if (input.checked) {
-                genderObj[input.id] = input.checked
-            }
-        }
-        else if (input.className !== "gender") {
-            formArr.push(input.value)
-        }
+// total objects array
+let allPersonsArray = []
+window.addEventListener('load', function () {
+    if (localStorage.length === 0) {
+        settingAllPersonsArrayToLocal(allPersonsArray)
     }
+})
+window.addEventListener('load', function () {
+    let innerArray = gettingAllPersonsArray()
+    let largeArray;
+    if (innerArray.length === 0) {
+        largeArray = innerArray
+    }
+    else {
+        let sortedArray = sortingArray(innerArray)
+        largeArray = sortedArray
+    }
+    for (let i = 0; i < (largeArray.length); i++) {
+        let editButton = document.createElement('button')
+        editButton.innerText = "Edit"
+        let deleteButton = document.createElement('button')
+        deleteButton.innerText = "Delete"
+        editButton.className = "edit"
+        deleteButton.className = "delete"
+        for (let detail in largeArray[i]) {
+            if (detail === "id") {
+                editButton.id = largeArray[i][detail]
+                deleteButton.id = largeArray[i][detail]
+            } else {
+
+                let h4 = document.createElement('h4')
+                h4.innerText = `${detail} ${largeArray[i][detail]}`
+                h4.id = largeArray[i].id
+                container.appendChild(h4)
+            }
+
+            container.appendChild(deleteButton)
+            container.appendChild(editButton)
+        }
+        editButton.addEventListener('click', function () {
+            let edtiButtonId = editButton.id
+            let arrayPersonValues
+            let indexOfPerson
+            // getting person from largeArray
+            for (let i = 0; i < largeArray.length; i++) {
+                for (let detail in largeArray[i]) {
+
+                    if (Number(edtiButtonId) === largeArray[i][detail]) {
+                        arrayPersonValues = largeArray[i]
+                        indexOfPerson = i
+                    }
+                }
+            }
+            //getting values from arrayPersonvalues
+            let personValues = []
+            for (let key in arrayPersonValues) {
+                personValues.push(arrayPersonValues[key])
+            }
+
+            //setting person to inputs
+            for (let i = 0; i < inputs.length; i++) {
+                inputs[i].value = personValues[i]
+            }
+
+            let removedPerson = largeArray.splice(indexOfPerson, 1)
+            settingAllPersonsArrayToLocal(largeArray)
+            for (let i = 0; i < 7; i++) {
+                let element = document.getElementById(edtiButtonId)
+                element.remove()
+            }
+
+
+        })
+        deleteButton.addEventListener('click', function () {
+            let deleteButtonId = deleteButton.id
+
+            let indexOfPerson
+            // getting person from largeArray
+            for (let i = 0; i < largeArray.length; i++) {
+                for (let detail in largeArray[i]) {
+                    if (Number(deleteButtonId) === largeArray[i][detail]) {
+                        indexOfPerson = i
+                    }
+                }
+            }
+            largeArray.splice(indexOfPerson, 1)
+            settingAllPersonsArrayToLocal(largeArray)
+            window.location.reload()
+        })
+    }
+}
+
+
+)
+submitButton.addEventListener("click", function (e) {
+    let allPersons = gettingAllPersonsArray()
+    person = creatingObjects(allPersons)
+
+    settingAllPersonsArrayToLocal(person)
     e.preventDefault()
+    removingFormValues()
+    window.location.reload()
+})
+
+// setting allpersonsArray  to localStorage
+function settingAllPersonsArrayToLocal(type) {
+    localStorage.setItem('allPersonsArray', JSON.stringify(type))
+}
+
+// getting allpersonsArray 
+function gettingAllPersonsArray() {
+
+    let arr = JSON.parse(localStorage.getItem('allPersonsArray'))
+    return arr
+}
+
+// creating objects with input data
+
+function creatingObjects(arr) {
+    let person = {}
+
+    let personDetails = ["First Name:", "Last Name:", "Dob:", "Mobile No:", "Email:"]
+    for (let i = 0; i < inputs.length; i++) {
+        person[personDetails[i]] = inputs[i].value
+
+    }
+    person["id"] = gettingLastPersonId(arr)
+
+
+    arr.push(person)
+    return arr
+}
+function gettingLastPersonId(arr) {
+    if (arr.length < 1) {
+        return 0
+    } else {
+        return Number(arr[arr.length - 1].id) + 1
+    }
+}
+
+function removingFormValues() {
     for (let input of inputs) {
-        input.value = ""
-        if (input.className === "gender") {
-            input.checked = false;
-        }
+        input.value = ''
     }
-    localStorage.setItem("userDataArr", JSON.stringify(formArr))
-    localStorage.setItem('userGenderObj', JSON.stringify(genderObj))
-    let userData = JSON.parse(localStorage.getItem("userDataArr"))
-    let userGender = JSON.parse(localStorage.getItem("userGenderObj"))
-    let details = ["First Name:", "Last Name:", "Date:", "Mobile No:", "Email:"]
-    h2.style.display = "block"
-    for (let i = 0; i < userData.length; i++) {
-        let h4 = document.createElement('h4')
-        h4.className = "fourth"
-        h4.innerText = `${details[i]}  ${userData[i]}`
-        container.appendChild(h4)
+}
 
+
+function sortingArray(array) {
+    let newArray = array.sort(compare)
+
+    function compare(a, b) {
+        let firstDate = new Date(a["Dob:"]); // Your timezone!
+        let secondDate = new Date(b["Dob:"])
+        let firstEpoch = firstDate.getTime() / 1000.0;
+        let secondEpoch = secondDate.getTime() / 1000.0;
+        return secondEpoch - firstEpoch
     }
-    for (let key in userGender) {
-        let h4 = document.createElement('h4')
-        h4.className = "fourth"
-        h4.innerText = `${key} : ${userGender[key]}`
-        container.appendChild(h4)
-
-    }
-    deleteButton.style.display = "block"
-    form.style.display = "none"
-    edit.style.display = "block"
-    document.body.appendChild(container)
-    container.style.display = "block"
-})
-edit.style.display = "none"
-
-
-
-edit.addEventListener('click', function () {
-    edit.style.display = "none"
-    let headingFourElements = document.querySelectorAll(".fourth")
-    form.style.display = "block"
-    for (let element of headingFourElements) {
-       
-        element.remove()
-    }
-
-    let userData = JSON.parse(localStorage.getItem("userDataArr"))
-    let userGender = JSON.parse(localStorage.getItem("userGenderObj"))
-    let i = 0
-    for (let input of inputs) {
-        input.value = userData[i]
-        i++
-        for (let key in userGender) {
-            if (input.id === key) {
-                input.checked = userGender[key]
-            }
-        }
-    }
-    genderObj = {}
-    formArr.length = 0
-    container.style.display = "none"
-})
-
-
-// function nameValidation(str, para) {
-//     if (str.length === 0 || (str.length >= 3 && str.length <= 25)) {
-//         para.style.display = "none"
-//         return true
-//     }
-//     else {
-//         para.innerText = "name must be greaterthan 3 characters and less than 25 characters"
-//         para.style.display = "block"
-//         return false
-//     }
-// }
-// function numberValidator(numberValue, para) {
-//     if ((numberValue > 10 && numberValue < 10) || isNaN(numberValu)) {
-//         para.style.display = "none"
-//         return true
-//     } else {
-//         para.innerText = "number must be equals to 10 digits"
-//         return false
-//     }
-// }
-
-deleteButton.addEventListener('click', function () {
-
-    localStorage.clear()
-
-    let headingFourElements = document.querySelectorAll(".fourth")
-    form.style.display = "block"
-    for (let element of headingFourElements) {
-
-        element.remove()
-    }
-    formArr.length = 0
-    genderObj = {}
-    localStorage.clear()
-    form.style.display = "block"
-    container.style.display = "none"
-})
-
-window.addEventListener('load',function(){
-    let userData = JSON.parse(localStorage.getItem("userDataArr"))
-    let userGender = JSON.parse(localStorage.getItem("userGenderObj"))
-    if(userData !== null){
-        utilityFunction()
-    }
-    else{
-        form.style.display = "block"
-    }
-})
-function utilityFunction(){
-    let userData = JSON.parse(localStorage.getItem("userDataArr"))
-    let userGender = JSON.parse(localStorage.getItem("userGenderObj"))
-    let details = ["First Name:", "Last Name:", "Date:", "Mobile No:", "Email:"]
-    h2.style.display = "block"
-    for (let i = 0; i < userData.length; i++) {
-        let h4 = document.createElement('h4')
-        h4.className = "fourth"
-        h4.innerText = `${details[i]}  ${userData[i]}`
-        container.appendChild(h4)
-
-    }
-    for (let key in userGender) {
-        let h4 = document.createElement('h4')
-        h4.className = "fourth"
-        h4.innerText = `${key} : ${userGender[key]}`
-        container.appendChild(h4)
-
-    }
-    deleteButton.style.display = "block"
-    form.style.display = "none"
-    edit.style.display = "block"
-    document.body.appendChild(container)
-    container.style.display = "block"
+    return newArray
 }
