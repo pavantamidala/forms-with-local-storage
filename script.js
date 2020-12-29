f6ac0e5b569c39c8f0a771b8e3a301537b603374
pavantamidala <pavantamidala023@gmail.com>
2020-12-30 00:46:33 +0530

    added validations

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ let inputs = document.querySelectorAll("input")
 let submitButton = document.querySelector(".submit")
 let container = document.querySelector(".container")
 let deleteAll = document.querySelector(".deleteall")
+let dateValidatorPara = document.querySelector(".date-validator")
+let firstNameValidatorPara = document.querySelector(".firstname-validator")
+let lastNameValidatorPara = document.querySelector(".lastname-validator")
+let numberValidatorPara = document.querySelector(".number-validator")
+let emaiValiatorPara = document.querySelector(".email-validator") 
+let validatorParas = document.querySelectorAll(".validator")
 // total objects array
 let allPersonsArray = []
 window.addEventListener('load', function () {
@@ -99,11 +105,17 @@ window.addEventListener('load', function () {
 submitButton.addEventListener("click", function (e) {
     let allPersons = gettingAllPersonsArray()
     person = creatingObjects(allPersons)
-
-    settingAllPersonsArrayToLocal(person)
+    console.log(typeof (person))
+    if (typeof (person) === "object") {
+        settingAllPersonsArrayToLocal(person)
+        e.preventDefault()
+        removingFormValues()
+        window.location.reload()
+    }
     e.preventDefault()
-    removingFormValues()
-    window.location.reload()
+    for(let para of validatorParas){
+        para.style.display = "block"
+    }
 })
 
 // setting allpersonsArray  to localStorage
@@ -125,14 +137,54 @@ function creatingObjects(arr) {
 
     let personDetails = ["First Name:", "Last Name:", "Dob:", "Mobile No:", "Email:"]
     for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].id === "date") {
+            if (dateValidator(inputs[i].value)) {
+                person[personDetails[i]] = inputs[i].value
+            }else{
+                inputs[i].addEventListener("click",function(){
+                    dateValidatorPara.style.display = "none"
+                })
+            }
+        }
+        else if(inputs[i].id ==="firstname"){
+            if(nameValidator(inputs[i].value,firstNameValidatorPara)){
+                person[personDetails[i]] = inputs[i].value
+            }else{
+                inputs[i].addEventListener("click",function(){
+                    firstNameValidatorPara.style.display = "none"
+                })
+            }
+        }else if(inputs[i].id ==="lastname"){
+            if(nameValidator(inputs[i].value,lastNameValidatorPara)){
+                    person[personDetails[i]] = inputs[i].value
+            }else{
+                inputs[i].addEventListener("click",function(){
+                    lastNameValidatorPara.style.display = "none"
+                })
+            }
+        }
+        else if(inputs[i].id ==="phone"){
+            if(numberValidator(inputs[i].value)){
+                person[personDetails[i]] = inputs[i].value
+            }else{
+                inputs[i].addEventListener("click",function(){
+                    numberValidatorPara.style.display = "none"
+                })
+            }
+        }
+        
+        if(inputs[i].id==="email"){
         person[personDetails[i]] = inputs[i].value
-
+        }
+        
     }
     person["id"] = gettingLastPersonId(arr)
-
-
-    arr.push(person)
-    return arr
+    
+    
+    if (Object.keys(person).length === 6) {
+        arr.push(person)
+        return arr
+    }
 }
 function gettingLastPersonId(arr) {
     if (arr.length < 1) {
@@ -148,7 +200,7 @@ function removingFormValues() {
     }
 }
 
-deleteAll.addEventListener("click",function(){
+deleteAll.addEventListener("click", function () {
     localStorage.clear()
     window.location.reload()
 })
@@ -165,3 +217,43 @@ function sortingArray(array) {
     }
     return newArray
 }
+
+function dateValidator(str) {
+    if (str.length === 0) {
+        dateValidatorPara.innerText = "date of birth required"
+        dateValidatorPara.style.background = "red"
+        sendingBackValuesToForm()
+        return false
+    }
+    else {
+        return true
+    }
+}
+function sendingBackValuesToForm(){
+    for (let inp of inputs) {
+        let val = inp.value
+        inp.value = val
+    }
+}
+function nameValidator(str,para){
+    if(str.length>=3 && str.length <=25){
+        return true
+    }else{
+        para.innerText = "name must be more than 3 characters and less than 25 characters "
+        para.style.background = "red"
+        sendingBackValuesToForm()
+        return false
+    }
+}
+
+function numberValidator(str){
+    if(String(str).length===10){
+        return true
+    }else{
+        numberValidatorPara.innerText = "Number must be 10 digits"
+        numberValidatorPara.style.background = "red"
+        sendingBackValuesToForm()
+        return false
+    }
+}
+
